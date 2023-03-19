ad4a970e1cf71e549726aab52dd8c21e4195ce08
Muhammad Awais <awaistopper20@gmail.com>
2023-03-19 14:16:35 +0500

    Created first view function

--- playground/views.py
+++ playground/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+# request -> response
+# request handler
+# action
+
+def say_hello(request):
+    # pull data from db
+    # Transform
+    # send email
+    return HttpResponse("Hello World")
