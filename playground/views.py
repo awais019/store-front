5c2a4d8618d7ceb1a0835596df1f4493f63f79f2
Muhammad Awais <awaistopper20@gmail.com>
2023-03-20 12:35:54 +0500

    Running raw sql queries

--- playground/views.py
+++ playground/views.py
@@ -1,19 +1,11 @@
 from django.shortcuts import render
-from django.db import transaction
+from django.db import connection
 from store.models import Order, OrderItem, Product, Customer, Collection
 
 def say_hello(request):
     
-    with transaction.atomic():
-        order = Order()
-        order.customer_id = 1
-        order.save()
-
-        item = OrderItem()
-        item.order = order
-        item.product_id = 1
-        item.quantity = 1
-        item.unit_price = 10
-        item.save()
+    # queryset = Product.objects.raw("SELECT * from store_product")
+    with connection.cursor() as cursor:
+        cursor.execute()
 
     return render(request, 'hello.html', { 'name': 'Awais' })
