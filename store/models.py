3671b131aea3f7ab7b0b0a8d74981da12de0296e
Muhammad Awais <awaistopper20@gmail.com>
2023-03-20 12:53:10 +0500

    Registering models

--- store/models.py
+++ store/models.py
@@ -9,6 +9,12 @@ class Promotion(models.Model):
 class Collection(models.Model):
     title = models.CharField(max_length=255)
     featured_product = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True, related_name='+')
+    
+    def __str__(self) -> str:
+        return self.title
+
+    class Meta:
+        ordering = ['title']
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
@@ -19,6 +25,12 @@ class Product(models.Model):
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
     promotions = models.ManyToManyField(Promotion)
+
+    def __str__(self) -> str:
+        return self.title
+
+    class Meta:
+        ordering = ['title'] 
 
 class Customer(models.Model):
     MEMBERSHIP_BRONZE = 'B'
