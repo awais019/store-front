33ecbf2c9a1d07e231dbbb2053ff4bb879f5f6c5
Muhammad Awais <awaistopper20@gmail.com>
2023-03-20 11:54:45 +0500

    Custom managers

--- playground/views.py
+++ playground/views.py
@@ -5,13 +5,6 @@ from tags.models import TaggedItem
 
 def say_hello(request):
     
-    content_type = ContentType.objects.get_for_model(Product)
-
-    queryset = TaggedItem.objects \
-            .select_related('tag') \
-                .filter(
-                content_type = content_type,
-                object_id = 1
-                )
+    queryset = TaggedItem.objects.get_tags_for(Product, 1)
 
     return render(request, 'hello.html', { 'name': 'Awais', 'Tags': list(queryset) })
--- tags/models.py
+++ tags/models.py
@@ -4,10 +4,22 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 
 # Create your models here.
 
+class TaggedItemManager(models.Manager):
+    def get_tags_for(self, obj_type, obj_id):
+        content_type = ContentType.objects.get_for_model(obj_type)
+
+        queryset = TaggedItem.objects \
+            .select_related('tag') \
+                .filter(
+                content_type = content_type,
+                object_id = obj_id
+                )
+        return queryset
 class Tag(models.Model):
     label = models.CharField(max_length=255)
 
 class TaggedItem(models.Model):
+    objects = TaggedItemManager()
     tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
