23ef7f26ef60d617392792535a3ef66be0ce653f
Muhammad Awais <awaistopper20@gmail.com>
2023-03-20 13:05:33 +0500

    Displaying computed columns

--- store/admin.py
+++ store/admin.py
@@ -4,9 +4,15 @@ from . import models
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'price']
+    list_display = ['title', 'price', 'inventory_status']
     list_editable = ['price']
     list_per_page = 10
+
+    @admin.display(ordering='inventory')
+    def inventory_status(self, product):
+        if product.inventory < 10:
+            return 'Low'
+        return 'Ok'
 
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
