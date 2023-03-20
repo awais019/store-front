29606d9cf711760ae4ae34fd832fecca3e027324
Muhammad Awais <awaistopper20@gmail.com>
2023-03-20 13:00:20 +0500

    Customizing list displays

--- store/admin.py
+++ store/admin.py
@@ -2,6 +2,19 @@ from django.contrib import admin
 from . import models
 # Register your models here.
 
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'price']
+    list_editable = ['price']
+    list_per_page = 10
+
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'membership']
+    list_editable = ['membership']
+    ordering = ['first_name', 'last_name']
+    list_per_page = 10
+
 admin.site.register(models.Collection)
 
-admin.site.register(models.Product)
+# admin.site.register(models.Product, ProductAdmin)
