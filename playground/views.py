29dd1dc3e7ee2a58669c863682247fbe3e2448e1
Muhammad Awais <awaistopper20@gmail.com>
2023-03-20 12:16:13 +0500

    Deleting objects

--- playground/views.py
+++ playground/views.py
@@ -4,10 +4,9 @@ from store.models import Order, OrderItem, Product, Customer, Collection
 
 def say_hello(request):
     
-    # collection = Collection.objects.get(pk=1)
-    # collection.featured_product = None
-    # collection.save()
+    collection = Collection(pk = 1)
+    collection.delete()
 
-    Collection.objects.filter(pk = 1).update(featured_product = None)
+    # Collection.objects.filter(id__gt = 5).delete()
 
     return render(request, 'hello.html', { 'name': 'Awais' })
