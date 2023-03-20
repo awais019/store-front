d93c34206ba6498ffdd391380a224bd496964d88
Muhammad Awais <awaistopper20@gmail.com>
2023-03-20 13:46:04 +0500

    Added related column of orders on customer page

--- store/admin.py
+++ store/admin.py
@@ -23,11 +23,18 @@ class ProductAdmin(admin.ModelAdmin):
 
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ['first_name', 'last_name', 'membership']
+    list_display = ['first_name', 'last_name', 'membership', 'orders_count']
     list_editable = ['membership']
     ordering = ['first_name', 'last_name']
     list_per_page = 10
 
+    def orders_count(self, customer):
+        return customer.orders_count
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(
+            orders_count = Count('order')
+        )
 @admin.register(models.Collection)
 class CollectionAdmin(admin.ModelAdmin):
     list_display = ['title', 'products_count']
