9ce66aa0cddc67de78a6d1d67c997839081221b7
Muhammad Awais <awaistopper20@gmail.com>
2023-03-20 16:00:33 +0500

    Serializing objects

--- store/views.py
+++ store/views.py
@@ -1,14 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404
 from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
+from rest_framework import status
+from .models import Product
+from .serializers import ProductSerializer
 # Create your views here.
 
 @api_view()
 def product_list(request):
-    return Response('ok')
+    query_set = Product.objects.all()
+    serializer = ProductSerializer(query_set, many=True)
+    return Response(serializer.data)
 
 @api_view()
 def product_detail(request, id):
-    return Response(id)
+    # try:
+    #     product = Product.objects.get(pk = id)
+    #     serializer = ProductSerializer(product)
+    #     return Response(serializer.data)
+    # except Product.DoesNotExist:
+    #     return Response(status=status.HTTP_404_NOT_FOUND)
+    product = get_list_or_404(Product, pk=id)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
