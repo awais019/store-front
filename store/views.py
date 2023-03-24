cfa2588e86923c9f9cd98279b654f6c9c88356ee
Muhammad Awais <awaistopper20@gmail.com>
2023-03-24 21:35:31 +0500

    Deleting a cart

--- store/views.py
+++ store/views.py
@@ -1,7 +1,7 @@
 from django.db.models import Count
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
-from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin
+from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, DestroyModelMixin
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
 from rest_framework.response import Response
 from rest_framework import status
@@ -55,6 +55,9 @@ class ReviewViewSet(ModelViewSet):
     def get_serializer_context(self):
         return { 'product_id': self.kwargs['product_pk'] }
 
-class CartViewSet(CreateModelMixin, RetrieveModelMixin, GenericViewSet):
+class CartViewSet(CreateModelMixin,
+                  RetrieveModelMixin,
+                  DestroyModelMixin,
+                  GenericViewSet):
     queryset = Cart.objects.prefetch_related('items__product').all()
     serializer_class = CartSerializer
