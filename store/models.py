90c17edf8972450a7b2dd57b2d35f9defa000629
Muhammad Awais <awaistopper20@gmail.com>
2023-03-25 11:28:00 +0500

    Added customer profile by extending user module

--- store/models.py
+++ store/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.conf import settings
+from django.contrib import admin
 from django.core.validators import MinValueValidator
 from uuid import uuid4
 # Create your models here.
@@ -44,18 +46,24 @@ class Customer(models.Model):
         (MEMBERSHIP_SILVER, 'Silver'),
         (MEMBERSHIP_GOLD, 'Gold'),
     ]
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
-    email = models.EmailField(unique=True)
     phone = models.CharField(max_length=255)
     birth_date = models.DateField(null=True)
     membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    @admin.display(ordering='user__first_name', description='First Name')
+    def first_name(self):
+        return self.user.first_name
+    
+    @admin.display(ordering='user__last_name', description='Last Name')
+    def last_name(self):
+        return self.user.last_name
 
     def __str__(self) -> str:
-        return f'{self.first_name} {self.last_name}'
+        return f'{self.user.first_name} {self.user.last_name}'
     
     class Meta:
-        ordering = ['first_name', 'last_name']
+        ordering = ['user__first_name', 'user__last_name']
 
 class Order(models.Model):
     PAYMENT_STATUS_PENDING = 'P'
