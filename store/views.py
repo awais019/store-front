e1a1e4ef60bc498831f4b8447ffff8bc88b31207
Muhammad Awais <awaistopper20@gmail.com>
2023-03-26 09:38:32 +0500

    Created customer profile end point.

--- store/views.py
+++ store/views.py
@@ -3,6 +3,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, DestroyModelMixin, UpdateModelMixin
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
+from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework import status
 
@@ -88,3 +89,15 @@ class CustomerViewSet(CreateModelMixin,
                     GenericViewSet):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
+
+    @action(detail=False, methods=['GET', 'PUT'])
+    def me(self, request):
+        (customer, created) = Customer.objects.get_or_create(user_id=request.user.id)
+        if request.method == 'GET':
+            serializer = CustomerSerializer(customer)
+            return Response(serializer.data)
+        elif request.method == 'PUT':
+            serializer = CustomerSerializer(customer, data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data)
