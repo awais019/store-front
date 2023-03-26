b22326b385e37b95702434ca6741d4bee9835f55
Muhammad Awais <awaistopper20@gmail.com>
2023-03-26 15:02:05 +0500

    Returning images in products.

--- store/serializers.py
+++ store/serializers.py
@@ -13,10 +13,22 @@ class CollectionSerializer(serializers.ModelSerializer):
     
     products_count = serializers.IntegerField(read_only=True)
 
+class ProductImageSerializer(serializers.ModelSerializer):
+
+    def create(self, validated_data):
+        product_id = self.context['product_id']
+        return ProductImage.objects.create(product_id=product_id, **validated_data)
+    class Meta:
+        model = ProductImage
+        fields = ['id', 'image']
+
 class ProductSerializer(serializers.ModelSerializer):
+    images = ProductImageSerializer(many=True, read_only=True)
     class Meta:
         model = Product
-        fields = ['id', 'title', 'description', 'slug', 'inventory', 'price', 'price_with_tax', 'collection']
+        fields = ['id', 'title', 'description',
+                  'slug', 'inventory', 'price',
+                  'price_with_tax', 'collection', 'images']
     # id = serializers.IntegerField()
     # title = serializers.CharField(max_length=255)
     # price = serializers.DecimalField(max_digits=6, decimal_places=2)
@@ -170,12 +182,3 @@ class CreateOrderSerializer(serializers.Serializer):
             Cart.objects.filter(pk=cart_id).delete()
             order_created.send_robust(self.__class__, order=order)
             return order
-        
-class ProductImageSerializer(serializers.ModelSerializer):
-
-    def create(self, validated_data):
-        product_id = self.context['product_id']
-        return ProductImage.objects.create(product_id=product_id, **validated_data)
-    class Meta:
-        model = ProductImage
-        fields = ['id', 'image']
--- store/views.py
+++ store/views.py
@@ -18,7 +18,7 @@ from .permissions import IsAdminOrReadOnly, ViewCustomerHistoryPermission
 # Create your views here.
 
 class ProductViewSet(ModelViewSet):
-    queryset = Product.objects.all()
+    queryset = Product.objects.prefetch_related('images').all()
     serializer_class = ProductSerializer
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_class = ProductFilter
