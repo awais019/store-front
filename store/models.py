8b2e697795a1076468d07b7c2360aa3a62543395
Muhammad Awais <awaistopper20@gmail.com>
2023-03-26 15:08:27 +0500

    Added validations for image files.

--- store/models.py
+++ store/models.py
@@ -3,6 +3,8 @@ from django.conf import settings
 from django.contrib import admin
 from django.core.validators import MinValueValidator
 from uuid import uuid4
+
+from .validators import validate_file_size
 # Create your models here.
 
 class Promotion(models.Model):
@@ -39,7 +41,7 @@ class Product(models.Model):
 
 class ProductImage(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
-    image = models.ImageField(upload_to='store/images')
+    image = models.ImageField(upload_to='store/images', validators=[validate_file_size])
 
 class Customer(models.Model):
     MEMBERSHIP_BRONZE = 'B'
