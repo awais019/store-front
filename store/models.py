20415bb077e8a7acbcae3c9e05498bfab274b48b
Muhammad Awais <awaistopper20@gmail.com>
2023-03-26 14:50:02 +0500

    Added data model for images.

--- store/models.py
+++ store/models.py
@@ -37,6 +37,10 @@ class Product(models.Model):
     class Meta:
         ordering = ['title'] 
 
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to='store/images')
+
 class Customer(models.Model):
     MEMBERSHIP_BRONZE = 'B'
     MEMBERSHIP_SILVER = 'S'
