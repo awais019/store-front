6d21b368e9f94fbdb7da901d42f3f1634f2ee09c
Muhammad Awais <awaistopper20@gmail.com>
2023-03-31 11:45:09 +0500

    Scheduling prediotic tasks with celery beat

--- storefront/settings.py
+++ storefront/settings.py
@@ -178,3 +178,10 @@ ADMINS = [
 ]
 
 CELERY_BROKER_URL = 'redis://localhost:6379/1'
+CELERY_BEAT_SCHEDULE = {
+    'notify_customers': {
+        'task': 'playground.tasks.notify_customers',
+        'schedule': 5,
+        'args': ['Hello World']
+    }
+}
