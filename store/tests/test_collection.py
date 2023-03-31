bbc3f9b65d48bff3aa863bde5ba76f21f6b5ca73
Muhammad Awais <awaistopper20@gmail.com>
2023-03-31 15:36:11 +0500

    Using models in testing

--- store/tests/test_collection.py
+++ store/tests/test_collection.py
@@ -1,6 +1,7 @@
-from django.contrib.auth.models import User
+from store.models import Collection
 from rest_framework import status
 import pytest
+from model_bakery import baker
 
 
 @pytest.fixture
@@ -45,3 +46,20 @@ class TestCreateCollection:
 
         assert response.status_code == status.HTTP_201_CREATED
         assert response.data['id'] > 0
+
+
+@pytest.mark.django_db
+class TestRetrieveCollection:
+    def test_if_collection_exists_returns_200(self, api_client):
+        # Arrange
+        collection = baker.make(Collection)
+
+        # Act
+        response = api_client.get(f'/store/collections/{collection.id}/')
+
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data == {
+            'id': collection.id,
+            'title': collection.title,
+            'products_count': 0,
+        }
